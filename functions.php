7e6993f52e69d27c54f35d5b9da2d7cc63799ea3
Andrey Petrov <petrovaa71@mail.ru>
2018-03-10 13:34:55 +0300

    сделал второе задание

--- functions.php
+++ functions.php
@@ -43,3 +43,63 @@ function task1()
         echo "<br>";
     }
 }
+
+function task2($data, $file1, $file2)
+{
+    writeJson($file1, $data);
+    $decoded = readJson($file1);
+    $change = randomChange($decoded, $file2);
+    if ($change) {
+        echo "<br>";
+        echo "Изменения есть!";
+        compare($file1, $file2);
+    } else {
+        echo "<br>";
+        echo "Изменений нет!";
+    }
+}
+
+function writeJson($file, $data)
+{
+    $encoded = json_encode($data);
+    file_put_contents($file, $encoded);
+}
+
+function readJson($file1)
+{
+    $json = file_get_contents($file1);
+    $decoded = json_decode($json, true);
+    return $decoded;
+}
+
+function randomChange($decoded, $file2)
+{
+    $rowRandom = rand(0, 5);
+    $columnRandom = rand(0, 5);
+    if ($rowRandom < 4 && $columnRandom < 3) {
+        $decoded[$rowRandom][$columnRandom] = "Изменен";
+        writeJson($file2, $decoded);
+        return true;
+    } else {
+
+        return false;
+    }
+}
+
+function compare($file1, $file2)
+{
+    $decoded1 = readJson($file1);
+    $decoded2 = readJson($file2);
+    for ($row = 0; $row < count($decoded1); $row++) {
+        for ($column = 0; $column < count($decoded1[$row]); $column++) {
+            if ($decoded1[$row][$column] != $decoded2[$row][$column]) {
+                echo "<br>";
+                echo "Значения не равны! Строека: " . $row . ". Столбец: " . $column . ".";
+                echo "<br>";
+                echo "В файле output.json значение: " . $decoded1[$row][$column];
+                echo "<br>";
+                echo "В файле output2.json значение: " . $decoded2[$row][$column];
+            }
+        }
+    }
+}
